c6934509ba1c37cd63e39de03b60ed3eca2d9170
Cem Aktaş <cmktass@gmail.com>
2021-08-11 14:14:26 +0300

    Add project files.

---
+++ BlogPageMvc/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using BlogPageMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPageMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+         public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
